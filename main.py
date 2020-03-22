51d5a7840e34fcc84699439252863ae0160ec6de
hts <1963410032@qq.com>
2020-03-23 00:26:31 +0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import base64,datetime,os,pymongo,hashlib,json
+import base64,datetime,os,pymongo,hashlib,json,requests
+import urllib
+
 from flask import Flask, request, url_for
 from pymongo import MongoClient
 from werkzeug.utils import secure_filename
@@ -54,6 +56,7 @@ def login():
 
 @app.route('/ImgUpload', methods=['POST'])
 def upload_pic():
+    # 用户头像上传——先不用
     if request.method == 'POST':
         token = request.form['token']
         username = request.form['username']
@@ -67,9 +70,23 @@ def upload_pic():
         file.close()
         return "ok"
     return "err"
-
+@app.route('/ImgUpload2', methods=['POST'])
+def upload_pic2():
+    #用户头像上传
+    if request.method == 'POST':
+        token = request.form['token']
+        username = request.form['username']
+        fileurl = request.form['imgurl']
+        loginTimeCmpAndClearUserToKen(token)
+    if fileurl:
+        mydata = mytb_login.find_one({"username": username})
+        if(len(mydata)>0):
+            mydata = mytb_login.update({'username': username},{'$set': {'imgurl': fileurl}})
+            return '{"ret":"ok","result":[{"data":"ok"}]}'
+    return '{"ret":"ok","result":[{"data":"err"}]}'
 @app.route('/my', methods=['POST'])
 def findMydata():
+    #得到我的数据
     if request.method == 'POST':
         token = request.form['token']
         username = request.form['username']
@@ -94,7 +111,41 @@ def updateMydata():
             return '{"Redirect":"login"}'
         mydata = mytb_login.update({'username':username},{'$set':{'age':age,'tel':tel,'birthday':birthday,'rmk':rmk}})
         if(len(mydata)>0):
-            return '{"ret":"ok","result":[{"data":"okdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdfgdg"}]}'
+            return '{"ret":"ok","result":[{"data":"ok"}]}'
     return '{"ret":"Redirect","result":[{"data":"login"}]}'
+@app.route('/manface', methods=['POST'])
+def manFace():
+    #人脸登录,手机端上传地址后跟原数据库中的地址上传到腾讯人脸识别上去比较发现》80的就可以登录
+    if request.method == 'POST':
+        username = request.form['username']
+        facepic = request.form['facepic']
+        mac = request.form['mac']
+        mydata = mytb_login.find_one({"username": username})
+        imgurl=""
+        password=""
+        if(len(mydata)>0):
+            del (mydata["_id"])
+            imgurl=mydata["imgurl"]
+            password=mydata["pw"]
+        if(goTenXunServerCmp(imgurl,facepic)>80):
+            #模拟登录
+            getPw = findPassword(username, password)
+            userToKen[getPw + '@' + mac] = int(datetime.datetime.now().strftime('%Y%m%d'))
+            return  '{"ret":"ok","result":[{"token":'+getPw+ '}]}'
+    return '{"ret":"Redirect","result":[{"data":"login"}]}'
+def goTenXunServerCmp(facepic1,facepic2):
+    #上传到腾讯人脸识别上去比较发现》80的就可以登录
+    headers = {}
+    headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:67.0) Gecko/20100101 Firefox/67.0'
+    headers['Accept'] = 'application/json'
+    headers['Content-Type'] = "application/x-www-form-urlencoded; charset=UTF-8"
+    data = {}
+    data['image_url_a'] = facepic1
+    data['image_url_b'] = facepic2
+    url = "https://ai.qq.com/cgi-bin/appdemo_facecompare?g_tk=5381"
+    reqdata = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(url, reqdata, headers)
+    html = urllib.request.urlopen(req).read()
+    return json.loads(html.decode('ascii'))['data']['similarity']
 if __name__ == '__main__':
-    app.run(debug=True,host='192.168.3.112',port=5000)
+    app.run(debug=True,host='192.168.3.253',port=5000)
---
+++ sql
@@ -0,0 +1,1 @@
+db.login.insert({username:"admin",pw:"123",age:"20",birthday:"2020-12-12",tel:"18608954340",rmk:"wo shi beizhu",imgurl:"sdfsdf"})
